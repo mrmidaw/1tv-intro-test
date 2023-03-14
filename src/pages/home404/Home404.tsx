9e4e8dbf9d96dd438020098a9617674aba20cf93
Vadim <mr.midaw@gmail.com>
2023-03-14 20:54:10 +0300

    404 page

--- src/pages/home404/Home404.tsx
+++ src/pages/home404/Home404.tsx
@@ -1,8 +1,45 @@
 import styles from "./Home404.module.scss";
-import React from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home404 = () => {
-  return <div>Home404</div>;
+  const navigate = useNavigate();
+
+  const [seconds, setSeconds] = useState<number>(10);
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 10000);
+
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.description}>
+        Кажется что-то пошло не так! Страница, которую вы запрашиваете, не
+        существует. Возможно она была удалена, или вы набрали неверный адрес.
+        Перейдите на &nbsp;
+        <Link to="/" className={styles.link}>
+          главную страницу
+        </Link>{" "}
+        и попробуйте найти необходимую информацию там. Автоматический переход
+        произойдет через {seconds}!
+      </p>
+    </div>
+  );
 };
 
 export default Home404;
